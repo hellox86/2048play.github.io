dca6014238e190f9346b9609a0ef35694475062b
hellox86 <zvonkov.dalv@gmail.com>
2026-01-15 23:44:59 +0800

    rules

--- scripts/game.js
+++ scripts/game.js
@@ -10,239 +10,255 @@ const font = "sans-serif";
 const ctx = canvas.getContext("2d");
 
 const tileColor = {
-    "2": "rgb(238, 228, 218)",
-    "4": "rgb(236, 224, 200)",
-    "8": "rgb(243, 178, 122)",
-    "16": "rgb(245, 149, 99)",
-    "32": "rgb(245, 124, 95)",
-    "64": "rgb(246, 93, 59)",
-    "128": "rgb(237, 206, 113)",
-    "256": "rgb(237, 204, 97)",
-    "512": "rgb(236, 200, 80)",
-    "1024": "rgb(237, 197, 63)",
-    "2048": "rgb(238, 194, 46)",
-    "default": "rgb(61, 58, 51)"
+  2: "rgb(238, 228, 218)",
+  4: "rgb(236, 224, 200)",
+  8: "rgb(243, 178, 122)",
+  16: "rgb(245, 149, 99)",
+  32: "rgb(245, 124, 95)",
+  64: "rgb(246, 93, 59)",
+  128: "rgb(237, 206, 113)",
+  256: "rgb(237, 204, 97)",
+  512: "rgb(236, 200, 80)",
+  1024: "rgb(237, 197, 63)",
+  2048: "rgb(238, 194, 46)",
+  default: "rgb(61, 58, 51)",
 };
 
-function fillCell(x, y, color, w=gridCellSize, h=gridCellSize) {
-    ctx.fillStyle = color;
-    ctx.fillRect(0.5 + x + offset, offset + y, w, h);
+function fillCell(x, y, color, w = gridCellSize, h = gridCellSize) {
+  ctx.fillStyle = color;
+  ctx.fillRect(0.5 + x + offset, offset + y, w, h);
 }
 function createCell(num, pos, textColor) {
-    const i = pos[0];
-    const j = pos[1];
-    
-    ctx.fillStyle = textColor;
-    ctx.strokeStyle = textColor;	    
-    ctx.lineWidth = 3;
-    const res = num.toString();
-    ctx.textBaseline = "middle";
-    ctx.textAlign = "center";
-    ctx.strokeText(res, offset + gridCellSize * j + gridCellSize/2, offset + gridCellSize * i + gridCellSize/2);
-    ctx.fillText(res, offset + gridCellSize * j + gridCellSize/2, offset + gridCellSize * i + gridCellSize/2);
+  const i = pos[0];
+  const j = pos[1];
+
+  ctx.fillStyle = textColor;
+  ctx.strokeStyle = textColor;
+  ctx.lineWidth = 3;
+  const res = num.toString();
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "center";
+  ctx.strokeText(
+    res,
+    offset + gridCellSize * j + gridCellSize / 2,
+    offset + gridCellSize * i + gridCellSize / 2,
+  );
+  ctx.fillText(
+    res,
+    offset + gridCellSize * j + gridCellSize / 2,
+    offset + gridCellSize * i + gridCellSize / 2,
+  );
 }
 
-export class GameField {    
-    #f;
-    #f2;    
-    count;
-    constructor (f) {
-	this.#f = f;
-	this.count = 0;
-    }
-    generateNum(times=1) {
-	let output;
-	const randomInRange = (min, max) => {
-	    const minCeiled = Math.ceil(min);
-	    const maxFloored = Math.floor(max);
-	    return Math.floor(Math.random()*(maxFloored-minCeiled+1) + minCeiled);
-	};
-	for (let i = 0; i < times; ++i) {
-	    output = 2;
-	    const randIndex = randomInRange(0, 9);
-	    
-	    if (randIndex == 9) {
-		output = 4;
-	    }
-	    let row;
-	    let col;
-	    let i = 0;
-	    
-	    do {
-		row = randomInRange(0, 3);
-		col = randomInRange(0, 3);
-		i++;
-	    } while(this.#f[row][col] != 0 && i < 35);
+export class GameField {
+  #f;
+  #f2;
+  count;
+  constructor(f) {
+    this.#f = f;
+    this.count = 0;
+  }
+  generateNum(times = 1) {
+    let output;
+    const randomInRange = (min, max) => {
+      const minCeiled = Math.ceil(min);
+      const maxFloored = Math.floor(max);
+      return Math.floor(
+        Math.random() * (maxFloored - minCeiled + 1) + minCeiled,
+      );
+    };
+    for (let i = 0; i < times; ++i) {
+      output = 2;
+      const randIndex = randomInRange(0, 9);
 
-	    if(i == 35) {	
-		return;
-	    }
-	    this.#f[row][col] = output;
-	    const currentColor = (output > 2) ? tileColor["4"] : tileColor["2"];
-	    const textColor = "rgb(117, 100, 82)"; 
-	    fillCell(100*col+5, 100*row+5, currentColor, 91, 91);
-	    ctx.font = `${textSize} ${font}`;
-	    createCell(this.#f[row][col], [row, col], textColor);
-	}
-    }   
-    draw() {
-	for (let x = 0; x <= fw; x += gridCellSize) {
-            ctx.moveTo(0.5 + x + offset, offset);
-            ctx.lineTo(0.5 + x + offset, fh + offset);
-	    for (let y = 0; y <= fh; y += gridCellSize) {
-		ctx.moveTo(offset, 0.5 + y + offset);
-		ctx.lineTo(fw + offset, 0.5 + y + offset);		
-	    }
-	}	
-	for (let x = 0; x < fw; x += gridCellSize) {
-	    for (let y = 0; y < fh; y += gridCellSize) {
-		fillCell(x, y, "rgb(184, 169, 156)");
-	    }	
-	}
-	ctx.lineWidth = 5;
-	ctx.strokeStyle = "rgb(156, 138, 124)";
-	ctx.stroke();
+      if (randIndex == 9) {
+        output = 4;
+      }
+      let row;
+      let col;
+      let i = 0;
 
-    }
-    reset() {
-	this.count = 0;
-	this.draw();
-	for (let i = 0; i < 4; ++i) {
-	    for (let j = 0; j < 4; ++j) {
-		this.#f[i][j] = 0;
-	    }
-	}	
-	this.generateNum(2);
-    }
-    getF() {
-	return this.#f; 
-    }
-    setF(obj) {
-	this.#f = obj;
-    }
-    update() {
-	this.draw();
-	for (let i = 0; i < 4; ++i) {
-	    for (let j = 0; j < 4; ++j) {
-		if (this.#f[i][j] != 0) {
-		    if (this.#f[i][j] > 2048) {
-			fillCell(100*j+5, 100*i+5, tileColor["default"], 90, 90);    
-		    } else {
-			fillCell(100*j+5, 100*i+5, tileColor[this.#f[i][j].toString()], 90, 90);			    	
-		    }
-		    ctx.font = `${textSize} ${font}`;
-		    const textColor = (this.#f[i][j] <= 4) ? "rgb(117, 100, 82)" : "rgb(255, 255, 255)";
-		    createCell(this.#f[i][j], [i, j], textColor);
-		}
-	    }
-	} 
-    }
-    moveLeft(f=true){
-	let arr = matrix.createField();
-	let el_counter = 0;	    
-	for (let i = 0; i < 4; ++i) {
-	    for (let j = 0; j < 4; ++j) {
-		if (this.#f[i][j] != 0) {
-		    arr[i][el_counter] = this.#f[i][j];
-		    ++el_counter;
-		}
-	    }
-	    el_counter = 0;	
-	}
-	let res = matrix.createField();
-	for (let i = 0; i < 4; ++i) {
-	    for (let j = 0; j < 4;) {
-		if (j+1 < 4 && arr[i][j] == arr[i][j+1]) {
-		    res[i][el_counter] = arr[i][j]*2;
-		    j+=2;
-		    this.count += res[i][el_counter];
-		} else {
-		    res[i][el_counter] = arr[i][j];
-		    ++j;
-		}
-		++el_counter;
-	    }
-	    el_counter = 0;
-	}
-	if (f) {
-	    this.#f = res.slice();	
-	} else {
-	    this.#f2 = res.slice();
-	}
-    }
-    cmp(prev, now) {
-	return matrix.compareTwoMatrix(prev, now);
-    }    
-    
-    moveRight() {
-	const prev = this.#f;
-	this.#f = matrix.rotate_180(this.#f);
-	this.moveLeft();
-	this.#f = matrix.rotate_180(this.#f);
-	const now = this.#f;
-	return this.cmp(prev, now);
-    }
-    
-    moveDown() {
-	const prev = this.#f;
-	this.#f = matrix.rotate_90cw(this.#f);
-	this.moveLeft();
-	this.#f = matrix.rotate_90ccw(this.#f);
-	const now = this.#f;
-	return this.cmp(prev, now);
-    }
+      do {
+        row = randomInRange(0, 3);
+        col = randomInRange(0, 3);
+        i++;
+      } while (this.#f[row][col] != 0 && i < 35);
 
-    moveUp() {
-	const prev = this.#f;
-	this.#f = matrix.rotate_90ccw(this.#f);
-	this.moveLeft();
-	this.#f = matrix.rotate_90cw(this.#f);
-	const now = this.#f;
-	return this.cmp(prev, now);
+      if (i == 35) {
+        return;
+      }
+      this.#f[row][col] = output;
+      const currentColor = output > 2 ? tileColor["4"] : tileColor["2"];
+      const textColor = "rgb(117, 100, 82)";
+      fillCell(100 * col + 5, 100 * row + 5, currentColor, 91, 91);
+      ctx.font = `${textSize} ${font}`;
+      createCell(this.#f[row][col], [row, col], textColor);
     }
-    isFull() {
-	for (let i = 0; i < 4; ++i) {
-	    for (let j = 0; j < 4; ++j) {
-		if(this.#f[i][j] == 0) {
-		    return false;
-		}
-	    }
-	}
-	return true;
+  }
+  draw() {
+    for (let x = 0; x <= fw; x += gridCellSize) {
+      ctx.moveTo(0.5 + x + offset, offset);
+      ctx.lineTo(0.5 + x + offset, fh + offset);
+      for (let y = 0; y <= fh; y += gridCellSize) {
+        ctx.moveTo(offset, 0.5 + y + offset);
+        ctx.lineTo(fw + offset, 0.5 + y + offset);
+      }
     }
-    isGameOver() {
-	const isMergeable = () => {
-	    this.moveLeft(false);	
-	    for (let i = 0; i < 4; ++i) {
-		for (let j = 0; j < 4; ++j) {
-		    if((j < 3 && this.#f2[i][j] == this.#f2[i][j+1]) ||
-		       (j > 0 && this.#f2[i][j] == this.#f2[i][j-1]) ||
-		       (i < 3 && this.#f2[i][j] == this.#f2[i+1][j] ||
-		       (i > 0 && this.#f2[i][j] == this.#f2[i-1][j])
-		       )
-		      )
-		      return false;
-		}		
-	    }
-	    return true;
-	}
-	if (this.isFull() && isMergeable()) {
-	    return true;
-	}
-	return false;
+    for (let x = 0; x < fw; x += gridCellSize) {
+      for (let y = 0; y < fh; y += gridCellSize) {
+        fillCell(x, y, "rgb(184, 169, 156)");
+      }
     }
-    print() {
-	let res = "";
-	for (let i = 0; i < 4; ++i) {
-	    for (let j = 0; j < 4; ++j) {
-		res += " " + this.#f[i][j];
-	    }
-	    console.log(res);
-	    console.log();
-	    res = "";
-	}
-	console.log();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "rgb(156, 138, 124)";
+    ctx.stroke();
+  }
+  reset() {
+    this.count = 0;
+    this.draw();
+    for (let i = 0; i < 4; ++i) {
+      for (let j = 0; j < 4; ++j) {
+        this.#f[i][j] = 0;
+      }
     }
+    this.generateNum(2);
+  }
+  getF() {
+    return this.#f;
+  }
+  setF(obj) {
+    this.#f = obj;
+  }
+  update() {
+    this.draw();
+    for (let i = 0; i < 4; ++i) {
+      for (let j = 0; j < 4; ++j) {
+        if (this.#f[i][j] != 0) {
+          if (this.#f[i][j] > 2048) {
+            fillCell(100 * j + 5, 100 * i + 5, tileColor["default"], 90, 90);
+          } else {
+            fillCell(
+              100 * j + 5,
+              100 * i + 5,
+              tileColor[this.#f[i][j].toString()],
+              90,
+              90,
+            );
+          }
+          ctx.font = `${textSize} ${font}`;
+          const textColor =
+            this.#f[i][j] <= 4 ? "rgb(117, 100, 82)" : "rgb(255, 255, 255)";
+          createCell(this.#f[i][j], [i, j], textColor);
+        }
+      }
+    }
+  }
+  moveLeft(f = true) {
+    let arr = matrix.createField();
+    let el_counter = 0;
+    for (let i = 0; i < 4; ++i) {
+      for (let j = 0; j < 4; ++j) {
+        if (this.#f[i][j] != 0) {
+          arr[i][el_counter] = this.#f[i][j];
+          ++el_counter;
+        }
+      }
+      el_counter = 0;
+    }
+    let res = matrix.createField();
+    for (let i = 0; i < 4; ++i) {
+      for (let j = 0; j < 4; ) {
+        if (j + 1 < 4 && arr[i][j] == arr[i][j + 1]) {
+          res[i][el_counter] = arr[i][j] * 2;
+          j += 2;
+          this.count += res[i][el_counter];
+        } else {
+          res[i][el_counter] = arr[i][j];
+          ++j;
+        }
+        ++el_counter;
+      }
+      el_counter = 0;
+    }
+    if (f) {
+      this.#f = res.slice();
+    } else {
+      this.#f2 = res.slice();
+    }
+  }
+  cmp(prev, now) {
+    return matrix.compareTwoMatrix(prev, now);
+  }
+
+  moveRight() {
+    const prev = this.#f;
+    this.#f = matrix.rotate_180(this.#f);
+    this.moveLeft();
+    this.#f = matrix.rotate_180(this.#f);
+    const now = this.#f;
+    return this.cmp(prev, now);
+  }
+
+  moveDown() {
+    const prev = this.#f;
+    this.#f = matrix.rotate_90cw(this.#f);
+    this.moveLeft();
+    this.#f = matrix.rotate_90ccw(this.#f);
+    const now = this.#f;
+    return this.cmp(prev, now);
+  }
+
+  moveUp() {
+    const prev = this.#f;
+    this.#f = matrix.rotate_90ccw(this.#f);
+    this.moveLeft();
+    this.#f = matrix.rotate_90cw(this.#f);
+    const now = this.#f;
+    return this.cmp(prev, now);
+  }
+  isFull() {
+    for (let i = 0; i < 4; ++i) {
+      for (let j = 0; j < 4; ++j) {
+        if (this.#f[i][j] == 0) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  isGameOver() {
+    const isMergeable = () => {
+      this.moveLeft(false);
+      for (let i = 0; i < 4; ++i) {
+        for (let j = 0; j < 4; ++j) {
+          if (
+            (j < 3 && this.#f2[i][j] == this.#f2[i][j + 1]) ||
+            (j > 0 && this.#f2[i][j] == this.#f2[i][j - 1]) ||
+            (i < 3 && this.#f2[i][j] == this.#f2[i + 1][j]) ||
+            (i > 0 && this.#f2[i][j] == this.#f2[i - 1][j])
+          )
+            return false;
+        }
+      }
+      return true;
+    };
+    if (this.isFull() && isMergeable()) {
+      return true;
+    }
+    return false;
+  }
+  print() {
+    let res = "";
+    for (let i = 0; i < 4; ++i) {
+      for (let j = 0; j < 4; ++j) {
+        res += " " + this.#f[i][j];
+      }
+      console.log(res);
+      console.log();
+      res = "";
+    }
+    console.log();
+  }
 }
 
 export const mem = matrix.createField();
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 import { GameField, mem } from "./game.js";
 
@@ -8,125 +8,123 @@ const f = localStorage.getItem("field");
 const score = localStorage.getItem("score");
 const restart = document.getElementById("restart");
 const count = document.getElementById("count");
+const howto = document.getElementById("howto");
 
-// TODO: touchpad handling in my game, animation
-
-function spawn(c=1) {
-    setTimeout(() => {
-	field.generateNum(c);
-    }, 150);
+function spawn(c = 1) {
+  setTimeout(() => {
+    field.generateNum(c);
+  }, 150);
 }
 
 if (f != null) {
-    field.setF(JSON.parse(f));
+  field.setF(JSON.parse(f));
+} else {
+  field.reset();
 }
 if (score != null) {
-    field.count = parseInt(score);
-    count.innerText = `Score: ${field.count}`;
+  field.count = parseInt(score);
+  count.innerText = `Очки: ${field.count}`;
 }
 field.update();
 restart.addEventListener("click", () => {
-    field.reset();
-    localStorage.setItem("field", JSON.stringify(field.getF()));
-    count.innerText = `Score: ${field.count}`;
-})
-
-function game(e) {     
-    if (e.key == "ArrowLeft" || e.key == "a") {
-	const prev = field.getF();
-	field.moveLeft();
-	const now = field.getF();
-	if(!field.cmp(prev, now)) {
-	    spawn();
-	}	
-    } else if ((e.key == "ArrowRight" || e.key == "d")) {
-	if(!field.moveRight()) {
-	    spawn();
-	}
-    } else if (e.key == "ArrowUp" || e.key == "w") {
-	if(!field.moveUp()) {
-	    spawn();
-	}
-    } else if (e.key == "ArrowDown" || e.key == "s") {
-	if(!field.moveDown()) {
-	    spawn();
-	}
+  field.reset();
+  localStorage.setItem("field", JSON.stringify(field.getF()));
+  count.innerText = `Очки: ${field.count}`;
+});
+howto.addEventListener("click", () => {
+  window.location.href = `/howto`;
+});
+function game(e) {
+  if (e.key == "ArrowLeft" || e.key == "a") {
+    const prev = field.getF();
+    field.moveLeft();
+    const now = field.getF();
+    if (!field.cmp(prev, now)) {
+      spawn();
     }
-    if (field.isGameOver()) {
-	alert("Game Over!");
-	field.reset();	
-	localStorage.setItem("field", JSON.stringify(field.getF()));
-	count.innerText = `Score: ${field.count}`;
-    } else {
-	count.innerText = `Score: ${field.count}`;
-	field.update();	
-    }   
+  } else if (e.key == "ArrowRight" || e.key == "d") {
+    if (!field.moveRight()) {
+      spawn();
+    }
+  } else if (e.key == "ArrowUp" || e.key == "w") {
+    if (!field.moveUp()) {
+      spawn();
+    }
+  } else if (e.key == "ArrowDown" || e.key == "s") {
+    if (!field.moveDown()) {
+      spawn();
+    }
+  }
+  if (field.isGameOver()) {
+    alert("Game Over!");
+    field.reset();
+  } else {
+    count.innerText = `Очки: ${field.count}`;
+    field.update();
+  }
 }
 
-let startX, startY = 0;
-let endX, endY = 0;
+let startX,
+  startY = 0;
+let endX,
+  endY = 0;
 
 document.addEventListener("touchstart", (e) => {
-    startX = e.changedTouches[0].clientX;
-    startY = e.changedTouches[0].clientY;
-})
+  startX = e.changedTouches[0].clientX;
+  startY = e.changedTouches[0].clientY;
+});
 
 function gameMobile(e) {
-    const deviationX = startX-endX;
-    const deviationY = startY-endY;
-    let flag = true;
-    if(Math.abs(deviationX) > Math.abs(deviationY)) {
-	flag = true;
-    } else if(Math.abs(deviationX) < Math.abs(deviationY)) {
-	flag = false;
+  const deviationX = startX - endX;
+  const deviationY = startY - endY;
+  let flag = true;
+  if (Math.abs(deviationX) > Math.abs(deviationY)) {
+    flag = true;
+  } else if (Math.abs(deviationX) < Math.abs(deviationY)) {
+    flag = false;
+  }
+  if (flag) {
+    if (deviationX > 0) {
+      const prev = field.getF();
+      field.moveLeft();
+      const now = field.getF();
+      if (!field.cmp(prev, now)) {
+        spawn();
+      }
+    } else if (deviationX < 0) {
+      if (!field.moveRight()) {
+        spawn();
+      }
     }
-    if(flag) {
-	if(deviationX > 0) {    
-	    const prev = field.getF();
-	    field.moveLeft();
-	    const now = field.getF();
-	    if(!field.cmp(prev, now)) {
-		spawn();
-	    }	
-	} else if(deviationX < 0) {
-	    if(!field.moveRight()) {
-		spawn();
-	    }
-	}
+  } else {
+    if (deviationY > 0) {
+      if (!field.moveUp()) {
+        spawn();
+      }
+    } else if (deviationY < 0) {
+      if (!field.moveDown()) {
+        spawn();
+      }
     }
-    else {	    
-	if(deviationY > 0) {
-	    if(!field.moveUp()) {
-		spawn();
-	    }
-	} else if(deviationY < 0) {
-	    if(!field.moveDown()) {
-		spawn();
-	    }
-	}
-    }
-if (field.isGameOver()) {
-	alert("Game Over!");
-	field.reset();	
-	localStorage.setItem("field", JSON.stringify(field.getF()));
-	count.innerText = `Score: ${field.count}`;
-    } else {
-	count.innerText = `Score: ${field.count}`;
-	field.update();	
-    }
-    
+  }
+  if (field.isGameOver()) {
+    alert("Game Over!");
+    field.reset();
+  } else {
+    count.innerText = `Очки: ${field.count}`;
+    field.update();
+  }
 }
 document.addEventListener("keydown", (e) => {
-    game(e);
-})
+  game(e);
+});
+
 document.addEventListener("touchend", (e) => {
-    endX = e.changedTouches[0].clientX;
-    endY = e.changedTouches[0].clientY;
-    gameMobile();
-})
-window.addEventListener("beforeunload", () => {	
-    localStorage.setItem("field", JSON.stringify(field.getF()));
-    localStorage.setItem("score", field.count);     
-})
-
-
+  endX = e.changedTouches[0].clientX;
+  endY = e.changedTouches[0].clientY;
+  gameMobile();
+});
+window.addEventListener("beforeunload", () => {
+  localStorage.setItem("field", JSON.stringify(field.getF()));
+  localStorage.setItem("score", field.count);
+});
